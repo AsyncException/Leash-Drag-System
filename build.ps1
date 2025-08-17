fe078884012b6c7aaa1fc3deb43312681cf89d39
Async Exception <152339337+AsyncException@users.noreply.github.com>
2025-08-17 14:54:41 +0200

    Made the Settings panel be able to scroll.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,14 @@
 $basePath = Split-Path -Parent $PSCommandPath
+$projectPath = "src\LDS\LDS.csproj"
+$versionPrefixMatch = "<VersionPrefix>(.*?)<\/VersionPrefix>"
+$versionSuffixMatch = "<VersionSuffix>(.*?)<\/VersionSuffix>"
+
+$projectContent = Get-Content -Path "$basePath\$projectPath"
+$versionPrefix = [Regex]::Match($projectContent, $versionPrefixMatch).Captures.Groups[1].Value;
+$versionSuffix = [Regex]::Match($projectContent, $versionSuffixMatch).Captures.Groups[1].Value
 
 Write-Host "Starting dotnet publish"
-dotnet publish "$basePath\src\LDS\LDS.csproj" `
+dotnet publish "$basePath\$projectPath" `
     --configuration Release `
     --runtime win-x64 `
     /p:PublishProfile= `
@@ -16,6 +23,6 @@ if($confirmation -eq "Y" -or $confirmation -eq "y"){
     $innoPath = "C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
     $innoConfig = ".\installer\Inno Setup Script.iss"
 
-    & "$innoPath" "/DBasePath=$basePath" $innoConfig
+    & "$innoPath" "/DBasePath=$basePath" "/DVersion=$versionPrefix-$versionSuffix" $innoConfig
 }
 

eb265aff1eb9cc2e737e2f29a0109de7502a5efe
Async Exception <152339337+AsyncException@users.noreply.github.com>
2026-08-11 22:58:22 +0200

    Removed TUI. This UI is unfortunately cancelled for a port to uno instead which should be linux compatible. Will be keeping the changes as this includes the seperation of the back end from the UI.

--- src/LDS.TUI/ServiceProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-public static class ServiceProvider
-{
-    public static IServiceProvider Services { get; private set; } = new ServiceCollection().BuildServiceProvider();
-    public static void Initializer(IServiceProvider services) => Services = services;
-}
